35572b2739e67ae5ae4b477a67738ef5587f1061
GodZzdl <198a@tut.edu.pl>
2016-01-07 16:34:31 +0800

    Update install_tomcat7.sh

--- install_tomcat7.sh
+++ install_tomcat7.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 chmod 777 uninstall_tomcat7.sh
 chmod 777 service.sh
-if [ ! -f "/root/jdk-7u79-linux-x64.gz" ]; then
+if [ ! -f "./jdk-7u79-linux-x64.gz" ]; then
  wget -O jdk-7u79-linux-x64.gz "https://xhzdxhtbsteduau-my.sharepoint.com/personal/198a_tut_edu_pl/_layouts/15/guestaccess.aspx?guestaccesstoken=fecV1GE7KtFOAXfmUUzOwofasUtfI93flXGyYHixmvM%3d&docid=02dd77d20e296405d885c6b86d409bbd3"
-  if [ ! -f "/root/jdk-7u79-linux-x64.gz" ]; then
+  if [ ! -f "./jdk-7u79-linux-x64.gz" ]; then
    echo "download faild try it again!"
    wget http://7u2tjs.com5.z0.glb.clouddn.com/linux/jdk/jdk-7u79-linux-x64.gz
   fi
@@ -15,9 +15,9 @@ mkdir /usr/lib/jvm
 tar zxvf ./jdk-7u79-linux-x64.gz  -C /usr/lib/jvm
 mv /usr/lib/jvm/jdk1.7.0_79 /usr/lib/jvm/jdk7
 
-if [ ! -f "/root/jdk.txt" ]; then
+if [ ! -f "./jdk.txt" ]; then
  wget -O jdk.txt "https://xhzdxhtbsteduau-my.sharepoint.com/personal/198a_tut_edu_pl/_layouts/15/guestaccess.aspx?guestaccesstoken=OnjYEdr%2b0d%2bMLgfLgSfwz03XnMNqBwuUa3%2fzUMiFYdk%3d&docid=0f4b256a7785e477aaa5d36d95a6d24c6"
-  if [ ! -f "/root/jdk.txt" ]; then
+  if [ ! -f "./jdk.txt" ]; then
    echo "download faild try it again!"
     wget http://7u2tjs.com5.z0.glb.clouddn.com/linux/jdk/jdk.txt
   fi
@@ -30,9 +30,9 @@ cat jdk.txt >> ~/.bashrc
 
 source ~/.bashrc
 
-if [ ! -f "/root/apache-tomcat-7.0.67.tar.gz" ]; then
+if [ ! -f "./apache-tomcat-7.0.67.tar.gz" ]; then
  wget -O apache-tomcat-7.0.67.tar.gz "https://xhzdxhtbsteduau-my.sharepoint.com/personal/198a_tut_edu_pl/_layouts/15/guestaccess.aspx?guestaccesstoken=L%2bpp16OGvDTutaHMc2p1wyVhY7lHqqrROHqdvJAzSzU%3d&docid=03e3782cde48b442fba1800c65120f72e"
-  if [ ! -f "/root/apache-tomcat-7.0.67.tar.gz" ]; then
+  if [ ! -f "./apache-tomcat-7.0.67.tar.gz" ]; then
    echo "download faild try it again!"
    wget http://7u2tjs.com5.z0.glb.clouddn.com/linux/jdk/apache-tomcat-7.0.67.tar.gz
   fi
@@ -47,5 +47,5 @@ chkconfig --add tomcat
 service tomcat start
 echo "your java home is /usr/lib/jvm/jdk7";
 echo "your tomcat home is /home/tomcat7";
-echo "use service tomcat start to run the tomcat7"
+echo "use service tomcat start/stop/restar to startup/shutdown/restart the tomcat7"
 echo "your default website is http://yourip:8080 enjoy it :)"
