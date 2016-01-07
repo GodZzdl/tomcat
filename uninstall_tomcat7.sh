0cc9b46a5c1ddc701fb10a641845490c0f5285a6
GodZzdl <198a@tut.edu.pl>
2016-01-07 16:35:19 +0800

    Update uninstall_tomcat7.sh

--- uninstall_tomcat7.sh
+++ uninstall_tomcat7.sh
@@ -15,9 +15,10 @@ if [ ! -d "/usr/lib/jvm" ]; then
     rm -rf jdk-7u79-linux-x64.gz
     rm -rf apache-tomcat-7.0.67.tar.gz
     rm -rf jdk.txt
-    echo "finish working"
+    echo "\033[32m finish working \033[0m"
   fi
  else
-  echo "deleted faild"
+  echo "\033[31m deleted faild \033[0m"
 fi
+
 
